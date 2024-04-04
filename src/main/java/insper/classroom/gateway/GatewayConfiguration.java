e90d498e2728f0e92b65c2f823f334a9a5513811
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2024-04-04 16:17:25 -0300

    att path gateway

--- src/main/java/insper/classroom/gateway/GatewayConfiguration.java
+++ src/main/java/insper/classroom/gateway/GatewayConfiguration.java
@@ -1,4 +1,4 @@
-package main.java.insper.classroom.gateway;
+package insper.classroom.gateway;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
--- src/main/java/insper/classroom/gateway/GatewayResource.java
+++ src/main/java/insper/classroom/gateway/GatewayResource.java
@@ -1,4 +1,4 @@
-package main.java.insper.classroom.gateway;
+package insper.classroom.gateway;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
